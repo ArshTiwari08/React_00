22d04760bc7862d7ebe85ec1c3ea4bece2545ba3
Arsh Tiwari <tiwariarsh348@gmail.com>
2026-08-15 15:14:49 +0530

    react_Route

--- React_rounter/src/App.jsx
+++ React_rounter/src/App.jsx
@@ -1,10 +1,18 @@
-import Header from './Component/Header/header'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
-  return (
-    <>
-      <Header/>
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
     </>
-  )
+    )
 }
+
 export default App
