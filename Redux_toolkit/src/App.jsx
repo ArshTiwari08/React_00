723909c002ad5e1dbe3f73320927e57e7adda4db
Arsh Tiwari <tiwariarsh348@gmail.com>
2026-08-19 12:09:50 +0530

    Redux_toolkit

---
+++ Redux_toolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
