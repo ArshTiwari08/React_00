f52f0880bb2f76efb5fa13a1f0faa49a034e221b
Arsh Tiwari <tiwariarsh348@gmail.com>
2026-09-16 13:00:09 +0530

    change

--- Megablog_Appwrite/src/pages/AllPost.jsx
+++ Megablog_Appwrite/src/pages/AllPost.jsx
@@ -1,17 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { Container, PostCard } from "../components";
 import service from "../Appwrite/config";
+import { Query } from "appwrite";
+import { useSelector } from "react-redux";
 
-function AllPost() {
+function MyPosts() {
     const [posts, setPosts] = useState([]);
 
+    const userData = useSelector(
+        (state) => state.auth.userData
+    );
+
     useEffect(() => {
-        service.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents);
-            }
-        });
-    }, []);
+        if (!userData?.$id) {
+            return;
+        }
+
+        service
+            .getPosts([
+                Query.equal("userId", userData.$id),
+                Query.equal("status", "active")
+            ])
+            .then((posts) => {
+                if (posts) {
+                    setPosts(posts.documents);
+                }
+            });
+    }, [userData]);
+
+    if (!userData) {
+        return (
+            <div className="w-full py-8 text-center">
+                <h1 className="text-2xl font-bold">
+                    Please login to see your posts
+                </h1>
+            </div>
+        );
+    }
+
+    if (posts.length === 0) {
+        return (
+            <div className="w-full py-8 text-center">
+                <Container>
+                    <h1 className="text-2xl font-bold">
+                        You have not created any posts yet.
+                    </h1>
+                </Container>
+            </div>
+        );
+    }
 
     return (
         <div className="w-full py-8">
@@ -20,7 +57,7 @@ function AllPost() {
                     {posts.map((post) => (
                         <div
                             key={post.$id}
-                            className="p-1 w-1/4"
+                            className="p-2 w-full sm:w-1/2 lg:w-1/4"
                         >
                             <PostCard post={post} />
                         </div>
@@ -31,4 +68,4 @@ function AllPost() {
     );
 }
 
-export default AllPost;
+export default MyPosts;
