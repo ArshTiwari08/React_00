8231c752d2f1cd048961ab4ba2133ead453c7903
Arsh Tiwari <tiwariarsh348@gmail.com>
2026-09-24 10:04:28 +0530

    change

--- Megablog_Appwrite/src/components/Header/Header.jsx
+++ Megablog_Appwrite/src/components/Header/Header.jsx
@@ -5,36 +5,36 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-    const authStatus =useSelector((state)=> state.auth.status)
-    const navigate = useNavigate()
+        const authStatus =useSelector((state)=> state.auth.status)
+        const navigate = useNavigate()
 
-    const navItems = [
-    {
-        name: 'Home',
-        slug: "/",
-        active: true
-    },
-    {
-        name: "Login",
-        slug: "/login",
-        active: !authStatus,
-    },
-    {
-        name: "Signup",
-        slug: "/signup",
-        active: !authStatus,
-    },
-    {
-        name: "My Posts",
-        slug: "/my-posts",
-        active: authStatus,
-    },
-    {
-        name: "Add Post",
-        slug: "/add-post",
-        active: authStatus,
-    },
-    ]
+        const navItems = [
+        {
+            name: 'Home',
+            slug: "/",
+            active: true
+        },
+        {
+            name: "Login",
+            slug: "/login",
+            active: !authStatus,
+        },
+        {
+            name: "Signup",
+            slug: "/signup",
+            active: !authStatus,
+        },
+        {
+            name: "My Posts",
+            slug: "/my-posts",
+            active: authStatus,
+        },
+        {
+            name: "Add Post",
+            slug: "/add-post",
+            active: authStatus,
+        },
+        ]
 
     return (
         <header className="py-3 shadow bg-gray-500">
